e76a7b904853fae9a15f7db7e8c61de74a11b4ea
PEDRO-APU <62312971+PEDRO-APU@users.noreply.github.com>
2020-03-25 19:51:23 -0300

    Archivos de proyecto

---
+++ EjerciciosColecciones/Operacion.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosColecciones
+{
+    public  class Operacion
+
+    {       
+
+       
+
+        public static void puntoAA(Dictionary<string, string> d)
+        {
+            foreach (KeyValuePair<string, string> pair in d)
+            {
+                if (pair.Key.Contains("Juan"))
+                {
+                    Console.WriteLine("Encontro: " + pair.Value);
+                }    
+                           
+            }
+            
+                  
+        }
+        public static void puntoA(Dictionary<string, string> d, String nombre)
+        {
+            if (d.ContainsKey(nombre))
+            {
+                Console.WriteLine("Encontro de llave: " + nombre + " :" + d[nombre]);
+            }
+            
+        }
+        public static void puntoB(Dictionary<string, string> d, string nombre)
+        {
+            String value = "";
+            //devuelve el valor asociado a esa clave.
+            if (d.TryGetValue(nombre, out value)){
+                Console.WriteLine("Encontro de llave: " + nombre + " :" + value);
+
+            }
+            else
+            {
+                Console.WriteLine("no contiene la llave: "+ nombre);
+            }
+        }
+        public static void recorrerDiccionario(Dictionary<string, string> d)
+        {
+            foreach (KeyValuePair<string, string> par in d) {
+                Console.WriteLine("Clave = {0}, Valor = {1}", par.Key, par.Value);
+            }       
+
+        }
+        public static void puntoD(Dictionary<string, string> d, String nombre, string telefono)
+        {
+            if (d.ContainsKey(nombre))
+            {
+                d[nombre] = telefono;
+                Console.WriteLine("Nuevo teléfono de mariana es: "  + d[nombre]);
+            }
+
+        }
+
+
+    }
+}
---
+++ EjerciciosColecciones/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosColecciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> values = new Dictionary<string, string>();
+
+            
+            values.Add("Juan", "55423412");
+            values.Add("Ernesto", "56985623");
+            values.Add("Mariana", "54787451");
+            //alternativa al punto a 
+            //Operacion.puntoAA(values);
+            //punto a
+            Operacion.puntoA(values, "Juan");
+            //punto b
+            Operacion.puntoB(values, "Pedro");
+            //punto c
+            Operacion.recorrerDiccionario(values);
+            //punto d
+            Operacion.puntoD(values, "Mariana", "58251425");
+            
+
+
+            Console.ReadKey();
+          
+
+        }
+    }
+}
